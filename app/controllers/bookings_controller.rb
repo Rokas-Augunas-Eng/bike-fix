fa16690eeb39d8940cd7a2edba061c5e2cf21194
rokas augunas <augunasr@gmail.com>
2021-08-17 18:40:46 +0100

    completed bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,52 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+  
+  def new
+  @booking = Booking.new 
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    find_mechanic
+    @booking.mechanic = @mechanic
+    if @booking.save
+      redirect_to profile_path
+    else
+      render :new  
+    end 
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @mechanic = @booking.mechanic
+    @booking.destroy
+    redirect_to mechanic_path(@mechanic)
+  end
+
+  def show
+    @bookings = Booking.find(params[:id])
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to mechanic_path(@mechanic)
+  end
+
+  private
+
+  def find_mechanic
+    @mechanic = mechanic.find(params[:mechanic_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :service_id, :booking_location, :comment, :datetime)
+  end
 end
