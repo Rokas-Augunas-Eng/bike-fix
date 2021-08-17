8084b1e49cebe81cfb747fedacc03f6c2dcb9425
rokas augunas <augunasr@gmail.com>
2021-08-17 18:19:46 +0100

    mechanics controller set up

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,2 +1,49 @@
 class MechanicsController < ApplicationController
+  before_action :set_mechanic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @mechanics = Mechanic.all
+  end
+
+  def new
+    @mechanic = Mechanic.new
+  end
+
+  def create
+    @mechanic = Mechanic.new(mechanic_params)
+    @mechanic.user = current_user
+    if @mechanic.save
+      redirect_to mechanic_path(@mechanic)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def edit
+  end
+
+  def update
+    @mechanic.update(mechanic_params)
+    redirect_to mechanic_path(@mechanic)
+  end
+
+  def destroy
+    @mechanic.destroy
+    redirect_to mechanics_path
+  end
+
+  private
+
+  def set_mechanic
+    @mechanic = Mechanic.find(params[:id])
+  end
+
+  def mechanic_params
+    params.require(:mechanic).permit(:location, :name, :insta_availability, :photo)
+  end
 end
