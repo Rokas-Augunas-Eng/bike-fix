0124b5f6752b3466590be61c170f96ff9bd4b843
rokas augunas <augunasr@gmail.com>
2021-08-17 19:08:48 +0100

    first seed population 1 user and 3 mechanics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'open-uri'
+
+Booking.destroy_all
+puts "Booking database deleted"
+
+Mechanic.destroy_all
+puts "Mechanic database deleted"
+
+User.destroy_all
+puts "User database deleted"
+
+# User creation
+
+user = User.create!([
+    email: "test@test.com",
+    password: "123456",
+  ])
+
+# Mechanic creation
+
+mechanic_1 = Mechanic.new(
+  location: "334 Walworth Rd, London",
+  name: "Bike Bros",
+  insta_availability: true,
+  user: User.first
+)
+
+mechanic_1.save!
+puts 'Created new mechanic'
+
+mechanic_2 = Mechanic.new(
+  location: "334 Walworth Rd, London",
+  name: "Bikes for days",
+  insta_availability: true,
+  user: User.first
+)
+
+mechanic_2.save!
+puts 'Created new mechanic'
+
+mechanic_3 = Mechanic.new(
+  location: "66 Gedling Pl, London",
+  name: "Get fix",
+  insta_availability: false,
+  user: User.first
+)
+
+mechanic_3.save!
+puts 'Created new mechanic'
+
+puts "Created #{User.count} users"
+puts "Created #{Mechanic.count} mechanics"
