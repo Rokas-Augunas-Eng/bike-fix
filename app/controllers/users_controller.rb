3cd78c0d804c91b620ff27c602236d7afef7a046
Robert Colvin <robert_colvin@hotmail.co.uk>
2021-08-17 21:53:21 +0100

    started on homepage backedn with new stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  
+
   def index
-    
+        @mechanics = Users.where('mechanic = true')
+
+        if params[:search_by_service].present?
+      @mechanics = @mechanics.services.where("repair_name ILIKE ?", "%#{params[:search_by_service]}%")
+    else
+      @mechanics
+    end
+  end
   end
 end
