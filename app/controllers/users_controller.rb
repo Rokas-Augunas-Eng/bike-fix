1c1d4dce5e4d406d048ee47e56c8c186366bf1ab
seanmeager <seanmeager10@gmail.com>
2021-08-18 17:59:55 +0100

    re-did the start of maps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   
   def index
-    
+    @users = User.all
+
+    # the `geocoded` scope filters only users with coordinates (latitude & longitude)
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
 end
